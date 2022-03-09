b2252967943f4dd03d1aafd652b17038fe55257a
MinhTT43 <minhtt43@gmail.com>
2022-03-09 16:40:58 +0100

    Attempt 1 adding asyncio

--- server.py
+++ server.py
@@ -1,37 +1,32 @@
-import socket
+from socket import (
+    socket,
+    AF_INET,
+    SOCK_STREAM
+)
 
-# Create a socket object
-# .AF_INET reefers to the address family IPv4
-# .SOCK_STREAM reefers to socket type TCP
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import asyncio
 
-# Assign IP-address and port for socket instance
-# associating sockets with determined network interface and port
-server_socket.bind(('127.0.0.1', 2345))
+async def client_handler(client):
+    loop = asyncio.get_event_loop()  # Define
+    request = None  # Data from client
+    while request != 'quit': # If user write "quit" exit
+        request = await loop.sock_recv(client, 255)
+        response = request.decode()
+        await loop.sock_sendall(client, response.encode())
 
-# Passive socket accepting incoming request with a given method
-server_socket.listen(1)
 
-# While-loop running until server is stopped
-while True:
-    print("Waiting...")  # Print message to console
+status = None
+server_socket = socket(AF_INET, SOCK_STREAM)  # Socket object for server
+server_socket.bind(('127.0.0.1', 2345))  # Assign IP-address and port
+server_socket.listen(4)
 
-    # Server listening for possible connections
-    # .accept used to establish connection creating a new socket object
-    client_socket, address = server_socket.accept()
+while status != 'quit':
+    print("Waiting...")
+    client, address = server_socket.accept()
+    print(f"Connection established - {address}")
+    client.send("Test".encode())
+    message_coded = client.recv(1024)
+    status = message_coded.decode()
+    print(status)
 
-    print(f"Connection established - {address}")  # Print message to console
-
-    # .send() sends response message from server socket to client sockets
-    client_socket.send("Connection has been esablished. Hello and goodbye".encode())
-
-    # Receive input from client
-    message = client_socket.recv(1024)
-
-    # Convert bytes to string
-    response = message.decode()
-
-    print("Input: " + response)
-
-    # .close() ends connection with client socket
-    #client_socket.close()
+client.close()
